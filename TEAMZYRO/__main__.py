2138c54944c1fc06f6886f1ecb5be7b61dc5244b
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-08-26 11:41:36 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,22 +1,34 @@
-from TEAMZYRO import *
 import importlib
 import logging
 from TEAMZYRO.modules import ALL_MODULES
+from telegram.ext import Application
+
+# === Logger ===
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
+
+# === Bot Token ===
+from TEAMZYRO.config import TOKEN   # apna token config.py me rakho
+
+# === Application ===
+application = Application.builder().token(TOKEN).build()
 
 
 def main() -> None:
+    # Load all modules
     for module_name in ALL_MODULES:
-        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
-    LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+        importlib.import_module("TEAMZYRO.modules." + module_name)
+    LOGGER.info("✅ All Features Loaded Baby🥳...")
 
-    ZYRO.start()
+    # Start bot
     application.run_polling(drop_pending_updates=True)
-    send_start_message()
-    LOGGER("TEAMZYRO").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+
+    LOGGER.info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎ MADE BY GOJOXNETWORK ☠︎︎ \n╚═════ஜ۩۞۩ஜ════╝"
     )
+
 
 if __name__ == "__main__":
     main()
-    
-    
